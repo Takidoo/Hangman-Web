89494ebf15e8d7993a96dc44a1574c6bab7a4e41
Taki Do <ferrusi.enzo06@gmail.com>
2024-11-18 10:20:30 +0100

    test

---
+++ hangmanModule/hangmanModule.go
@@ -0,0 +1,83 @@
+package hangmanModule
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+type PageData struct {
+	WordToFind string
+	Try        int
+	Endd       bool
+	Phrase     string
+}
+
+var Word string
+var CurrentWord string
+var Rcw []rune
+var HangData PageData
+var Essais int
+var Fin bool
+var EndSentence string
+
+func InitWordList(file string) string {
+	var l []string
+	f, err := os.Open(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		l = append(l, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return l[rand.Intn(len(l))]
+}
+
+func InitGame() {
+	fmt.Printf("\x1bc")
+	Word = InitWordList(os.Args[1])
+	Essais = 10
+	Fin = false
+	EndSentence, CurrentWord = "", ""
+	for i := 0; i < len(Word); i++ {
+		CurrentWord += "_"
+	}
+	Rcw = []rune(CurrentWord)
+}
+
+func CheckLettre(lettre string) {
+	if Word == lettre {
+		Fin = true
+		EndSentence = "Bravo, vous avez gagné !"
+	} else {
+		if strings.Contains(Word, lettre) {
+			for i, l := range Word {
+				if lettre == string(l) {
+					Rcw[i] = l
+				}
+			}
+		} else {
+			Essais = Essais - 1
+		}
+	}
+	if Essais == 0 {
+		Fin = true
+		EndSentence = "Dommage, vous avez perdu :("
+	} else if string(Rcw) == Word {
+		Fin = true
+		EndSentence = "Bravo, vous avez gagné !"
+	}
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"HangmanWeb/hangmanModule"
+	"html/template"
+	"net/http"
+)
+
+func hangHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		hangmanModule.CheckLettre(r.FormValue("lettre"))
+	}
+	tmpl, _ := template.ParseFiles("hangman.html")
+	hangmanModule.HangData = hangmanModule.PageData{
+		WordToFind: string(hangmanModule.Rcw),
+		Try:        hangmanModule.Essais,
+		Endd:       hangmanModule.Fin,
+		Phrase:     hangmanModule.EndSentence,
+	}
+	tmpl.Execute(w, hangmanModule.HangData)
+}
+
+func restart(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("reset.html")
+	hangmanModule.InitGame()
+	tmpl.Execute(w, hangmanModule.HangData)
+
+}
+
+func main() {
+	hangmanModule.InitGame()
+	http.HandleFunc("/", hangHandler)
+	http.HandleFunc("/reset", restart)
+	http.ListenAndServe(":80", nil)
+}
